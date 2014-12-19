bd06c4f8fd2b26e7a96c926e0c57c5158db240db
Jonathan <Jonny@Charless-iMac.lan>
2014-12-19 22:28:27 +0000

    Functional tests now checks we can input a to do item

--- functional_tests.py
+++ functional_tests.py
@@ -1,4 +1,5 @@
 from selenium import webdriver
+from selenium.webdriver.common.keys import Keys 
 import unittest
 
 class NewVisitorTest(unittest.TestCase):
@@ -13,18 +14,32 @@ class NewVisitorTest(unittest.TestCase):
 		#User checks out the webpage
 		self.browser.get('http://localhost:8000')
 
-		#user notices that page title and header mention to-do lists
-		self.assertIn('To-Do',self.browser.title) 
-		self.fail('Finish the test!')
-
+		#user notices that page title AND header mention to-do lists
+		self.assertIn('To-Do',self.browser.title)
+		header_text = self.browser.find_element_by_tag_name('h1').text
+		self.assertIn('To-Do',header_text)
 
 		#she is invited to enter a to-do item straight away
+		inputbox = self.browser.find_element_by_id('id_new_item')
+		self.assertEqual(
+			inputbox.get_attribute('placeholder'),
+			'Enter a to-do item'
+			)
 
 		#she types buy slippers
+		inputbox.send_keys('Buy slippers')
 
 		#she hits enter, the page updates, and now page lists buy peakcock feathers
+		inputbox.send_keys(Keys.ENTER)
+
+		table = self.browser.find_element_by_id('id_list_table')
+		rows = table.find_elements_by_tag_name('tr')
+		self.assertTrue(
+			any(row.text == '1.: Buy slippers')
+			)
 
 		#still a text box inviting her to add a another item
+		self.fail('Finish the functional fucking test!')
 
 		#page udates and npw shows both items
 
