b20dd771072e2f33aaef019e32f6abfc93f879de
Jonathan <Jonny@Charless-iMac.lan>
2014-12-19 21:54:20 +0000

    First unit test and url mapping, dummy view. pg30

--- lists/tests.py
+++ lists/tests.py
@@ -1,5 +1,7 @@
 from django.test import TestCase
 from django.core.urlresolvers import resolve
+from django.http import HttpRequest
+
 from lists.views import home_page
 # Create your tests here.
 
@@ -11,3 +13,12 @@ class HomePageTest(TestCase):
 	def test_root_url_resolves_to_home_page_view(self):
 		found = resolve('/')
 		self.assertEqual(found.func, home_page)
+
+	def test_home_page_returns_correct_html(self):
+		request = HttpRequest()
+		response = home_page(request)
+		# check the response we send from home_page in lists.views starts witg an html tag that is closed
+		self.assertTrue(response.content.startswith(b'<html>')) # b is for bytes, have to use to compare
+		# check we have a title tag with the words to do in it
+		self.assertIn(b'<title>To-Do lists</title>', response.content)
+		self.assertTrue(response.content.endswith(b'</html>'))
