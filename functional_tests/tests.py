6dd1805bb816d34bf513ff8f2de9d95774736612
Jonathan <Jonny@Charless-iMac.lan>
2014-12-21 20:25:12 +0000

    "make functional tests an add(folder), and chanded ft.py to tests.py inside it. Now use djnagos liverserver test case instead of unit test. This therefore cleans up after iteslf on the dtat base. Not usre how this works, but seperates from tests from real world database"

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -1,8 +1,8 @@
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys 
-import unittest
+from django.test import LiveServerTestCase
 
-class NewVisitorTest(unittest.TestCase):
+class NewVisitorTest(LiveServerTestCase):
 	def setUp(self):
 	    self.browser = webdriver.Firefox()
 	    self.browser.implicitly_wait(3) #to make sure selenium waits for pages to complete loading
@@ -19,7 +19,8 @@ class NewVisitorTest(unittest.TestCase):
 
 	def test_can_start_a_list_and_retrieve_it_later(self):
 		#User checks out the webpage
-		self.browser.get('http://localhost:8000')
+		#self.browser.get('http://localhost:8000') # for use with import unittest and unittest.unitest? in the class that newvisitor inherits
+		self.browser.get(self.live_server_url)
 
 		#user notices that page title AND header mention to-do lists
 		self.assertIn('To-Do',self.browser.title)
