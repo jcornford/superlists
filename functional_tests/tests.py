6fb88d75b2bbb25c85d79da944bd2da1bf636dc6
Jonathan <Jonny@Charless-iMac.lan>
2014-12-22 14:37:19 +0000

    "Changes to Ft, not looks for second user and new url for each user"

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -40,11 +40,10 @@ class NewVisitorTest(LiveServerTestCase):
 		inputbox.send_keys('Buy slippers')
 
 		#When she hits enter, she is taken to a new URL, and now page lists it.
-   
-
-		#she hits enter, the page updates, and now page lists buy peakcock feathers
 		inputbox.send_keys(Keys.ENTER)
-
+		edith_list_url  = self.browser.current_url
+		self.assertRegex(edith_list_url, '/lists/.+') # assert regex is a helper function from unnitest
+		# that checks whether a string matches a regualr expression.
 		self.check_for_row_in_list_table('1: Buy slippers')
 
 		
@@ -62,8 +61,37 @@ class NewVisitorTest(LiveServerTestCase):
 
 		#she visits that url, her to do list is still there
 
-		#she fucks off
+		#she fucks of
 
+		#Now a new user comes to the site, and make sure no information is coming through cookies.
+		## We use a new browser session to make sure no info from edith comes from cookies etc
+		self.browser.quit()
+		self.browser = webdriver.Firefox()
+
+		#user2 visits the homepage, no sign of ediths shit.
+		self.browser.get(self.live_server_url)
+		page_text = self.browser.find_element_by_tag_name('body').text
+		self.assertNotIn('Buy slippers', page_text)
+		self.assertNotIn('Put on slippers', page_text)
+
+		#user2 starts entering more shit.
+		inputbox = self.browser.find_element_by_id('id_new_item')
+		inputbox.send_keys('Take a shit')
+		inputbox.send_keys(Keys.ENTER)
+
+		#user2 gets nis hown url
+		user2_list_url  = self.browser.current_url
+		self.assertRegex(user2_list_url, '/lists/.+') # assert regex is a helper function from unnitest
+		# that checks whether a string matches a regualr expression.
+		self.assertNotEqual(user2_list_url, edith_list_url)
+
+		#Again, check that him entering has not added ediths stuff somehow.
+		self.browser.get(self.live_server_url)
+		page_text = self.browser.find_element_by_tag_name('body').text
+		self.assertNotIn('Buy slippers', page_text)
+		self.assertIn('Take a shit', page_text) # and his is there
+
+		
 
 
 		self.fail('Finish the functional fucking test!')
