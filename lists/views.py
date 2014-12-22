fd68fd4ab00e11f12ea291f14d281b006d2a3e1a
Jonathan <Jonny@Charless-iMac.lan>
2014-12-22 17:39:01 +0000

    End 95

--- lists/views.py
+++ lists/views.py
@@ -5,11 +5,6 @@ from lists.models import Item
 
 # Create your views here.
 def home_page(request):
-	if request.method == 'POST':
-		new_item_text = request.POST['item_text'] # obviously don't need to hold as a variable first
-		Item.objects.create(text=new_item_text) # this saves the item
-		return redirect('/lists/the-only-list-in-the-world/')
-
 	return render(request, 'home.html')
 
 def view_list(request):
