81e9d324fa602b6f38806255efbd34fba8f0d389
Jonathan <Jonny@Charless-iMac.lan>
2014-12-22 19:44:22 +0000

    pg.105.5

--- lists/tests.py
+++ lists/tests.py
@@ -14,6 +14,30 @@ from lists.models import Item, List
 
 # these tests shoud be about testing logic, flow control, and configuration. Not really `HTML contstants...
 class NewListTest(TestCase):
+	def test_can_save_a_POST_request_to_an_existing_list(self):
+		# dont understand this
+		other_list = List.objects.create()
+		correct_list = List.objects.create()
+		self.client.post(
+			'/lists/%d/add_item' % (correct_list.id,)
+			data = {'item_text':'A new item for exisiting list'}
+			)
+		self.assertEqual(Item.objects.count(),1)
+		new_item = Item.objects.first()
+		self.assertEqual(new_item.text, 'A new item for an existing list')
+		self.assertEqual(new_item.text, correct_list)
+
+	def test_redirects_to_list_view(self):
+		other_list = List.objects.create()
+		correct_list = List.objects.create()
+		response = self.client.post(
+			'/lists/%d/add_item' % (correct_list.id,)
+			data = {'item_text':'A new item for exisiting list'}
+			)
+		self.assertRedirects(response, '/lists/%d/' % (correct_list.id,))
+
+
+
 	def test_saving_a_POST_request(self):
 		
 		self.client.post(
