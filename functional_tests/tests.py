daa1d5d5468ae9e649e6bfdb6818b6e305989608
Jonathan <Jonny@Charless-iMac.lan>
2014-12-22 20:32:10 +0000

    new URL +view for adding to exisitng lists. FT passes. p.g.110

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -58,13 +58,6 @@ class NewVisitorTest(LiveServerTestCase):
 		self.check_for_row_in_list_table('1: Buy slippers')
 		self.check_for_row_in_list_table('2: Put on slippers')
 
-		#user wonders whether the page will remeber her list, she then sees that the 
-		#site has generated a unqiue earl for her -- there is some text
-
-		#she visits that url, her to do list is still there
-
-		#she fucks of
-
 		#Now a new user comes to the site, and make sure no information is coming through cookies.
 		## We use a new browser session to make sure no info from edith comes from cookies etc
 		self.browser.quit()
@@ -78,35 +71,37 @@ class NewVisitorTest(LiveServerTestCase):
 
 		#user2 starts entering more shit.
 		inputbox = self.browser.find_element_by_id('id_new_item')
+		self.assertEqual(
+			inputbox.get_attribute('placeholder'),
+			'Enter a to-do item'
+			)
 		inputbox.send_keys('Take a shit')
 		inputbox.send_keys(Keys.ENTER)
 
 		#user2 gets nis hown url
+
 		user2_list_url  = self.browser.current_url
 		self.assertRegex(user2_list_url, '/lists/.+') # assert regex is a helper function from unnitest
 		# that checks whether a string matches a regualr expression.
 		self.assertNotEqual(user2_list_url, edith_list_url)
 
 		#Again, check that him entering has not added ediths stuff somehow.
-		self.browser.get(self.live_server_url)
+		self.browser.get(self.browser.current_url)
 		page_text = self.browser.find_element_by_tag_name('body').text
 		self.assertNotIn('Buy slippers', page_text)
-		self.assertIn('Take a shit', page_text) # and his is there
+		self.assertIn('Take a shit', page_text,page_text) # and his is there
 
-		
-
-
-		self.fail('Finish the functional fucking test!')
 
 
 		
 
-		
+'''		
 
 if __name__ =='__main__':
 	#call unittest.main(), which launches the unittest test runner, which finds test
 	#classes and methods ijn the file and runs them.
 	unittest.main(warnings='ignore')
+'''
 
 
 
--- lists/views.py
+++ lists/views.py
@@ -14,8 +14,7 @@ def home_page(request):
 
 def view_list(request, list_id):
 	list_ = List.objects.get(id=list_id)
-	items = Item.objects.filter(list = list_) # used so can be added to the return render
-	return render(request, 'list.html', {'items':items})
+	return render(request, 'list.html', {'list':list_})
 
 def new_list(request):
 	list_ = List.objects.create()
