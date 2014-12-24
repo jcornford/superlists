fc505c32a1055cb1285b89f870f7f0d2e5a97a92
jonathan53 <Jonathan@Jonathans-Air.lan>
2014-12-24 01:53:23 +0000

    css working

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -3,10 +3,12 @@
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys 
 from django.test import LiveServerTestCase
+from django.contrib.staticfiles.testing import StaticLiveServerTestCase
 
 
 
-class NewVisitorTest(LiveServerTestCase):
+#class NewVisitorTest(LiveServerTestCase):
+class NewVisitorTest(StaticLiveServerTestCase):
 
 	def test_layout_and_styling(self):
 		#eidth goes to home page
